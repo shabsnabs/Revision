e78759dd7b14379ab508c36e2c0683548cbe40ed
shabsnabs <41385102+shabsnabs@users.noreply.github.com>
2023-04-24 09:53:01 +0530

    addition
    Added comment to display "MONEY"

--- MAven/src/main/java/MacMaven/BankAccount.java
+++ MAven/src/main/java/MacMaven/BankAccount.java
@@ -17,7 +17,7 @@ public class BankAccount {
 
 	public String  decision() {
 		Scanner obj2 = new Scanner(System.in);
-		System.out.println("Enter whether to Withdraw or Deposit ");
+		System.out.println("Enter whether to Withdraw or Deposit Money");
 		String wd = obj2.nextLine();
 		// int deposit=obj2.nextInt();
 		if (wd.equals("Withdraw")) {
